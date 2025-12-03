c7f4ea945a37f321933f2d504b2a0a4747650dab
devyy <devyyj@gmail.com>
2025-12-03 21:57:39 +0900

    초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // 개발 시작을 위한 최소한의 컴포넌트만 남깁니다.
+    <div style={{padding: '20px'}}>
+      <h1>Hello, MUI React App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
